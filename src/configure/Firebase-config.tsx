ded51d9d08fff340bfe38eee62cb1c7d0224ffef
Ezekiel Ajayi <106255357+altramax@users.noreply.github.com>
2023-05-11 15:18:25 +0100

    Update Firebase-config.tsx

--- src/configure/Firebase-config.tsx
+++ src/configure/Firebase-config.tsx
@@ -3,7 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBl6LbTP0VtK1L_rH971oQn-qvM7OJ8GTw",
+  authDomain: "fir-course-445b9.firebaseapp.com",
+  projectId: "fir-course-445b9",
+  storageBucket: "fir-course-445b9.appspot.com",
+  messagingSenderId: "242399488545",
+  appId: "1:242399488545:web:ed363eca88f2138922d160",
+  measurementId: "G-QN0P27PV5P"
+};
 
 
 const app = initializeApp(firebaseConfig);
